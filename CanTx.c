dd4bfb105f782ba08c58b5d2f7f01645a9545266
Soham Ghosh <57453010+Sghoshxperia@users.noreply.github.com>
2020-09-07 13:23:49 +0530

    Create CanTx.c

---
+++ CanTx.c
@@ -0,0 +1,44 @@
+#include "stm32f10x.h"
+
+uint32_t value = 0x42;
+
+void CAN_Transmit(uint32_t data)
+{
+	CAN1 ->sTxMailBox[0].TDTR |= (1<<0);
+	CAN1 ->sTxMailBox[0].TIR |= (1<<24) | (1<<22);
+	CAN1 ->sTxMailBox[0].TDLR = data;
+	CAN1 ->sTxMailBox[0].TIR |= (1<<0);
+	
+	while(~(CAN1 ->TSR & 0x01)); 
+}
+
+int main()
+{
+	
+	RCC ->APB1ENR |= RCC_APB1ENR_CAN1EN;
+	RCC ->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN;
+	
+	GPIOA ->CRH |= GPIO_CRH_CNF12_1 | GPIO_CRH_MODE12;
+	GPIOA ->CRH &= ~GPIO_CRH_CNF12_0;
+	GPIOA ->CRH &= ~GPIO_CRH_MODE11;
+	
+	CAN1 ->MCR |= CAN_MCR_INRQ;
+	
+	CAN1->BTR|=(1<<3);                                       // Prescaler=9                      
+	CAN1->BTR|=(1<<19)|(1<<18);                              // Seg1=13
+	CAN1->BTR|=(1<<20);                                      // Seg2=2
+	
+	CAN1 ->MCR &= ~(unsigned)(CAN_MCR_INRQ);
+	while((CAN1 ->MCR & 0x01) == 1);
+	
+	while(1)
+	{
+		CAN_Transmit(value);
+		for(int i = 0; i<100000; i++ );
+		
+	}
+	
+	
+	
+}
+
